af6e2ca45aafb0171252da2e35a735e2f5302565
Andrea <andrea@kreichgauer.com>
2021-06-24 04:14:33 +0200

    refactor dotbase properties

--- src/main/java/ca/uhn/fhir/jpa/starter/dotbase/DotbaseProperties.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotbase/DotbaseProperties.java
@@ -8,16 +8,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties
 public class DotbaseProperties {
-  private Boolean resolve_external_references = true;
+
+  private Boolean authentication_interceptor_enabled = true;
   private Boolean error_monitoring_enabled = true;
+  private String identity_provider_realm = "";
+  private String realm_public_key = "";
+  private Boolean resolve_external_references = true;
   private String server_name= "dotbase FHIR Server";
 
-  public Boolean getResolveExternalReferences() {
-    return resolve_external_references;
+  public Boolean  getAuthenticationInterceptorEnabled() {
+    return authentication_interceptor_enabled;
   }
 
-  public void setResolveExternalReferences(Boolean resolve_external_references) {
-    this.resolve_external_references = resolve_external_references;
+  public void  setAuthenticationInterceptorEnabled(Boolean authentication_interceptor_enabled) {
+    this.authentication_interceptor_enabled = authentication_interceptor_enabled;
   }
 
   public Boolean getError_monitoring_enabled() {
@@ -27,7 +31,30 @@ public class DotbaseProperties {
   public void setError_monitoring_enabled(Boolean error_monitoring_enabled) {
     this.error_monitoring_enabled = error_monitoring_enabled;
   }
+  public String getIdentityProviderRealm() {
+    return identity_provider_realm;
+  }
 
+  public void setIdentityProviderRealm(String identity_provider_realm) {
+    this.identity_provider_realm = identity_provider_realm;
+  }
+
+  public String getRealmPublicKey() {
+    return realm_public_key;
+  }
+
+  public void setRealmPublicKey(String realm_public_key) {
+    this.realm_public_key = realm_public_key;
+  }
+
+  public Boolean getResolveExternalReferences() {
+    return resolve_external_references;
+  }
+
+  public void setResolveExternalReferences(Boolean resolve_external_references) {
+    this.resolve_external_references = resolve_external_references;
+  }
+  
   public String getServer_name() {
     return server_name;
   }
@@ -35,5 +62,4 @@ public class DotbaseProperties {
   public void setServer_name(String server_name) {
     this.server_name = server_name;
   }
-
 }
