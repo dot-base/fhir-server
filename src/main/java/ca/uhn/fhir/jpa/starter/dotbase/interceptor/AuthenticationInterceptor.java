217901df80889e852920173750453804280693b1
Andrea <andrea@kreichgauer.com>
2021-06-24 04:44:41 +0200

    enable AuditTrail on transactions

--- src/main/java/ca/uhn/fhir/jpa/starter/dotbase/interceptor/AuthenticationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotbase/interceptor/AuthenticationInterceptor.java
@@ -3,7 +3,7 @@ package ca.uhn.fhir.jpa.starter.dotbase.interceptor;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Pointcut;
 // import ca.uhn.fhir.jpa.starter.dotbase.services.AccessLog;
-// import ca.uhn.fhir.jpa.starter.dotbase.services.AuditTrail;
+import ca.uhn.fhir.jpa.starter.dotbase.services.AuditTrail;
 import ca.uhn.fhir.jpa.starter.dotbase.services.Authentication;
 import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
@@ -17,7 +17,7 @@ public class AuthenticationInterceptor {
   private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(
     AuthenticationInterceptor.class
   );
-//   private static final String PROCESSING_SUB_REQUEST = "BaseHapiFhirDao.processingSubRequest";
+  private static final String PROCESSING_SUB_REQUEST = "BaseHapiFhirDao.processingSubRequest";
 
   @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
   public void preHandleIncomingRequest(
@@ -26,9 +26,9 @@ public class AuthenticationInterceptor {
     RestOperationTypeEnum restOperationType
   ) {
     String username = "unknown";
-    // boolean isTransaction = restOperationType.equals(RestOperationTypeEnum.TRANSACTION);
-    // boolean isSubRequest =
-    //   theRequestDetails.getUserData().get(PROCESSING_SUB_REQUEST) == Boolean.TRUE;
+    boolean isTransaction = restOperationType.equals(RestOperationTypeEnum.TRANSACTION);
+    boolean isSubRequest =
+      theRequestDetails.getUserData().get(PROCESSING_SUB_REQUEST) == Boolean.TRUE;
     boolean isAuthenticated = theRequestDetails.getAttribute("_username") != null;
 
     if (!isAuthenticated) {
@@ -38,11 +38,11 @@ public class AuthenticationInterceptor {
     //   AccessLog.logRequest(username, theRequestDetails, restOperationType);
     }
 
-    //
-    // if (isTransaction && !isSubRequest) {
-    //   AuditTrail.handleTransaction(theRequestDetails);
-    //   AccessLog.handleTransaction(username, theRequestDetails);
-    // }
+    
+    if (isTransaction && !isSubRequest) {
+      AuditTrail.handleTransaction(theRequestDetails);
+      // AccessLog.handleTransaction(username, theRequestDetails);
+    }
   }
 
   /**
