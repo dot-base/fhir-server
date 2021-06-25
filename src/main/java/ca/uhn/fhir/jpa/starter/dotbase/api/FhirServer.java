a908676e3fe027b686a5ddf33e9eb769bad3e14a
Andrea <andrea@kreichgauer.com>
2021-06-25 10:47:22 +0200

    add authorization with x-forwared-user to api/fhir

--- src/main/java/ca/uhn/fhir/jpa/starter/dotbase/api/FhirServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotbase/api/FhirServer.java
@@ -10,6 +10,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public class FhirServer {
   private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(
@@ -26,23 +27,30 @@ public class FhirServer {
       .build();
   }
 
-  private static String getAuthHeader(RequestDetails theRequestDetails) {
-    if (theRequestDetails.getHeader("Authorization") != null) {
-      return theRequestDetails.getHeader("Authorization");
+   /**
+   * Currently Authorization is not set on incoming requests. Thus, we retrieve the username from
+   * header "X-Forwarded-User" for the moment.
+   */
+  private static ImmutablePair<String, String> getAuthHeader(RequestDetails theRequestDetails) {
+    ImmutablePair<String,String> authHeader = new ImmutablePair<>("","");
+    if (theRequestDetails.getHeader("X-Forwarded-User") != null) {
+      return new ImmutablePair<>("X-Forwarded-User", theRequestDetails.getHeader("X-Forwarded-User"));
     }
-    return "";
+    if (theRequestDetails.getHeader("Authorization") != null) {
+      return new ImmutablePair<>("Authorization", theRequestDetails.getHeader("Authorization"));
+    }
+    return authHeader;
   }
 
   public static String getExternalResource(String uri, RequestDetails theRequestDetails) {
     HttpResponse<String> response = null;
-    String authHeader = getAuthHeader(theRequestDetails);
-
+    ImmutablePair<String, String> authHeader = getAuthHeader(theRequestDetails);
     HttpRequest request = HttpRequest
       .newBuilder()
       .uri(URI.create(uri))
       .setHeader("Content-Type", "application/fhir+json")
       .setHeader("Accept", "application/fhir+json; fhirVersion=4.0")
-      .setHeader("Authorization", authHeader)
+      .setHeader(authHeader.getKey(),authHeader.getValue())
       .GET()
       .build();
     try {
