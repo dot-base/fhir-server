f726af39e5a94bb44a261a402c58f3b7cebf06fb
Andrea <andrea@kreichgauer.com>
2021-06-25 15:40:01 +0200

    fix timezone offset and datetime format

--- src/main/java/ca/uhn/fhir/jpa/starter/dotbase/utils/DateUtils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotbase/utils/DateUtils.java
@@ -8,20 +8,21 @@ public class DateUtils {
   private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(DateUtils.class);
 
   public static String getCurrentTimestamp() {
-    TimeZone localTimeZone = getTimeZone();
-    String offSet = getOffSet_UTC(localTimeZone);
-    SimpleDateFormat utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss" + offSet);
-    utcFormat.setTimeZone(localTimeZone);
+    TimeZone timeZone = getTimeZone();
+    String offSet = getOffSet_UTC(timeZone);
+    SimpleDateFormat utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"+offSet);
     return utcFormat.format(new Date());
   }
 
   private static TimeZone getTimeZone() {
-    if (TimeZone.getDefault() == null) return TimeZone.getTimeZone("UTC");
+    if (TimeZone.getDefault() == null){
+      return TimeZone.getTimeZone("UTC");
+    }
     return TimeZone.getDefault();
   }
 
   private static String getOffSet_UTC(TimeZone localTimeZone) {
-    int rawOffSet = localTimeZone.getRawOffset() / 3600000 + 1;
+    int rawOffSet = localTimeZone.getRawOffset() / 3600000;
     String operator = rawOffSet >= 0 ? "+" : "-";
     return operator + String.format("%02d", rawOffSet) + ":00";
   }
