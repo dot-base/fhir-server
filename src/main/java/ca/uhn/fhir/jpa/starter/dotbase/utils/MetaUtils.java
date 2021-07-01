cd0cba7d926ed16c2b133a5c2a37c3a06abc7048
Andrea <andrea@kreichgauer.com>
2021-07-01 12:51:41 +0200

    enforce unique value for meta.tag.code

--- src/main/java/ca/uhn/fhir/jpa/starter/dotbase/utils/MetaUtils.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotbase/utils/MetaUtils.java
@@ -3,9 +3,8 @@ package ca.uhn.fhir.jpa.starter.dotbase.utils;
 import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
 import ca.uhn.fhir.context.BaseRuntimeElementCompositeDefinition;
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.jpa.starter.dotbase.DotbaseProperties.ResourceUrls;
-
 import java.util.List;
+import java.util.Random;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseCoding;
 import org.hl7.fhir.instance.model.api.IBaseMetaType;
@@ -15,7 +14,7 @@ public class MetaUtils {
   private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(MetaUtils.class);
 
   public static void setTag(FhirContext theContext, IBaseResource theResource, String theSystem, String theValue) {
-    
+
     IBaseMetaType theMeta = theResource.getMeta();
     BaseRuntimeElementCompositeDefinition<?> elementDef = (BaseRuntimeElementCompositeDefinition<?>) theContext
         .getElementDefinition(theMeta.getClass());
@@ -29,23 +28,24 @@ public class MetaUtils {
       sourceChild.getMutator().setValue(theMeta, tagElement);
     }
 
-
-
-    String theCode = MetaUtils.getTagElementCode(theSystem, theValue);
+    String theCode = getTagElementCode(theValue);
     tagElement.setSystem(theSystem);
     tagElement.setDisplay(theValue);
     tagElement.setCode(theCode);
   }
-  
-  private static String getTagElementCode(String theSystem, String theValue) {
-    if(enforceUniqueCode(theSystem)) {
-      theValue+="-"+DateUtils.getCurrentTimestamp();
-    }
+
+  /**
+   * --- Generates a unique value for meta.tag.code --- Otherwise duplicate values
+   * in table hfj_tag_def occure, which leads to HTTP 500 being returned with
+   * error message "ConstraintViolationException: could not execute batch"
+   */
+  private static String getTagElementCode(String theValue) {
+    theValue += "-" + DateUtils.getCurrentTimestamp() + "-" + enforceUniqueCode();
     return theValue;
   }
 
-  private static boolean enforceUniqueCode(String theSystem) {
-    return theSystem.equals(ResourceUrls.namingsystem_dotbase_username);
+  private static Long enforceUniqueCode() {
+    return new Random().nextLong();
   }
-  
+
 }
